27104d39d683eb5e6c401b326a12a13cab81e487
Jagan Karan <jagan.karan13@gmail.com>
2020-06-12 11:18:46 +0530

    added ssl configuration to the kafka client

--- src/configuration/KafkaClient.ts
+++ src/configuration/KafkaClient.ts
@@ -1,22 +1,26 @@
-import { Kafka } from 'kafkajs';
-
+import { Kafka } from "kafkajs";
+import * as fs from "fs";
 /**
  * Kafka Client Class.
  */
 export class KafkaClient {
-
-    /**
-     * Function which returns kafka configurations.
-     */
-    public kafkaProperties(): Kafka {
-        // Kafka Configuration Settings.
-        const kafka: Kafka = new Kafka({
-            // Kafka Client id
-            clientId: 'kafka-producer',
-            // List of brokers available and can be used in the function
-            brokers: ['localhost:9092']
-        })
-        return kafka;
-    }
-
+  /**
+   * Function which returns kafka configurations.
+   */
+  public kafkaProperties(): Kafka {
+    // Kafka Configuration Settings.
+    const kafka: Kafka = new Kafka({
+      // Kafka Client id
+      clientId: "kafka-producer",
+      // List of brokers available and can be used in the function
+      brokers: ["localhost:9093"],
+      ssl: {
+        rejectUnauthorized: false,
+        ca: [fs.readFileSync("/home/jagan/Desktop/Nasco/SSL/ca-cert.pem")],
+        passphrase: "qwerty",
+        cert: fs.readFileSync("/home/jagan/Desktop/Nasco/SSL/cert-signed.pem"),
+      },
+    });
+    return kafka;
+  }
 }
